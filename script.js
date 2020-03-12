5a2e1ba1b2d67003b75054655ec7672768eee729
Jose Aniceto <joseaniceto@gmail.com>
2020-03-12 11:03:34 +0000

    adding starting project

---
+++ script.js
@@ -0,0 +1,35 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var botao = document.getElementById("btn");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function randomColor() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ "#" 
+	+ Math.floor(Math.random()*16777215).toString(16)
+	+ ", " 
+	+ "#"
+	+ Math.floor(Math.random()*16777215).toString(16) 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+botao.addEventListener("click", randomColor);
